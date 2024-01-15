016a6a45c6ffd7cc2893580d9573a1b83c587b54
Maxime Regeard <regeard@apc.in2p3.fr>
2024-01-15 17:47:06 +0100

    first commit
    Signed-off-by: Maxime Regeard <regeard@apc.in2p3.fr>

---
+++ src/okkie/phase/phasing.py
@@ -0,0 +1,70 @@
+from astropy.time import Time
+import logging
+import numpy as np
+
+log = logging.getLogger(__name__)
+
+
+Class CPhaseMaker:
+
+    def __init__(
+            self,
+            pint_model, 
+            observatory, 
+            errors, 
+            ephem="DE421", 
+            include_bipm=True, 
+            include_gps=True,
+            planets=True,
+            ):
+
+        self._pint_model = pint_model
+        self.observatory = observatory
+        self.errors = errors
+        self.ephem = ephem
+        self.include_bipm = include_bipm
+        self.include_gps = include_gps
+        self.planets = planets
+
+    @property
+    def pint_model(self):
+        return self._pint_model
+
+    @pint_model.setter
+    def pint_model(self, model):
+        #TODO: check that the model is an instance of pint model
+        self._pint_model = model
+
+    def run(self, observation):
+        time = self._check_times(observation)
+        toas = toa.get_TOAs_array(
+                time=time,
+                obs=self.observatory,
+                errors=self.errors,
+                ephem=self.ephem,
+                include_bipm=self.include_bipm,
+                inlude_gps=self.include_gps,
+                planets=self.planets,
+                )
+
+        phases = self.model.phase(toas, abs_phase=True)[1]
+        phases = np.where(phases < 0.0, phases + 1.0, phases)
+
+        return phases
+
+    def _check_times(self, observation):
+        time = observation.events.time
+        time_min = time.min().tt.mjd
+        time_max = time.max().tt.mjd
+
+        model_time_range = Time(
+                [self.model.START.value, self.model.FINISH.value], 
+                scale='tt', 
+                format='mjd'
+                )
+
+        if time_min < model_time_range.value or time_max > model_time_range.value:
+            log.warning(f"At least one of the time of observation: {observation.obs_id} is outside of the validity range of the timing model")
+        return time
+
+
