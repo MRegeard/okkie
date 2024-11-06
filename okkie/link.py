fa5dc391a69e1252c72ecf0bd7ff63299d2c6080
Maxime Regeard <regeard@apc.in2p3.fr>
2024-11-06 15:27:37 +0100

    link gammapy

---
+++ okkie/link.py
@@ -0,0 +1,19 @@
+from gammapy.modeling.models import MODEL_REGISTRY
+from okkie.modeling.models import SourceModel
+
+PACKAGE = ["gammapy"]
+
+
+def link(package_name):
+    """Use to link okkie functionality to other packages classes that okkie classes inherit from.
+    This is particularly usefull for deserialisation.
+
+    Parameters
+    ----------
+    package_name: `str`
+        Name of package, {gammapy}.
+    """
+    if package_name.lower() not in PACKAGE:
+        raise ValueError(f"Nothing to link for package {package_name}.")
+    if package_name.lower() == "gammapy":
+        MODEL_REGISTRY.append(SourceModel)
