16479a6cd8d865f70a360e711021d35609ba65c5
Maxime Regeard <regeard@apc.in2p3.fr>
2024-11-16 01:43:39 +0100

    move setup dependencies to pyproject + pyproject build

--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name="okkie",
-    version="0.0.1",
-    packages=find_packages(
-        include=[
-            "astropy",
-            "gammapy>=1.2",
-            "matplotlib>=3.4",
-            "scipy!=1.10",
-            "iminuit>=2.8.0",
-            "regions>=0.5",
-            "numpy>1.20",
-            "pint-pulsar~=0.9.3",
-            "ruff",
-        ]
-    ),
-)
+setup()
