5f1664ded611917f7b492fa92189c2493c4553d6
Maxime Regeard <regeard@apc.in2p3.fr>
2025-04-14 00:51:45 +0200

    add PLEC4 3PC

---
+++ okkie/modeling/models/spectral.py
@@ -0,0 +1,66 @@
+import numpy as np
+from gammapy.modeling import Parameter
+from gammapy.modeling.models import SpectralModel
+
+
+class SuperExpCutoffPowerLaw3PCSpectralModel(SpectralModel):
+    r"""Spectral super exponential cutoff power-law model used for 3PC.
+
+    Parameters
+    ----------
+    amplitude : `~astropy.units.Quantity`
+        :math:`\phi_0`.  Default is 1e-12 cm-2 s-1 TeV-1.
+    reference : `~astropy.units.Quantity`
+        :math:`E_0`. Default is 1 TeV.
+    expfactor : `~astropy.units.Quantity`
+        :math:`a`, given as dimensionless value. Default is 1e-2.
+    index_1 : `~astropy.units.Quantity`
+        :math:`\Gamma_1`. Default is 1.5.
+    index_2 : `~astropy.units.Quantity`
+        :math:`\Gamma_2`. Default is 2.
+    """
+
+    tag = ["SuperExpCutoffPowerLaw4FGLDR3SpectralModel", "secpl-4fgl-dr3"]
+    amplitude = Parameter(
+        name="amplitude",
+        value="1e-12 cm-2 s-1 TeV-1",
+        scale_method="scale10",
+        interp="log",
+    )
+    reference = Parameter("reference", "1 TeV", frozen=True)
+    expfactor = Parameter("expfactor", "1e-2")
+    index_1 = Parameter("index_1", 1.5)
+    index_2 = Parameter("index_2", 2)
+
+    @staticmethod
+    def evaluate(energy, amplitude, reference, expfactor, index_1, index_2):
+        """Evaluate the model (static function)."""
+        # https://fermi.gsfc.nasa.gov/ssc/data/analysis/scitools/source_models.html#PLSuperExpCutoff4
+        pwl = amplitude * (energy / reference) ** (-index_1)
+        cutoff = (energy / reference) ** (expfactor / index_2) * np.exp(
+            expfactor / index_2**2 * (1 - (energy / reference) ** index_2)
+        )
+
+        return pwl * cutoff
+
+    @property
+    def e_peak(self):
+        r"""Spectral energy distribution peak energy (`~astropy.units.Quantity`).
+
+        This is the peak in E^2 x dN/dE and is given by Eq. 21 of https://iopscience.iop.org/article/10.3847/1538-4357/acee67:
+
+        .. math::
+            E_{Peak} = E_{0} \left[1+\frac{\Gamma_2}{a}(2 - \Gamma_1)\right]^{\frac{1}{\Gamma_2}}
+        """
+        reference = self.reference.quantity
+        index_1 = self.index_1.quantity
+        index_2 = self.index_2.quantity
+        expfactor = self.expfactor.quantity
+        index_0 = index_1 - expfactor / index_2
+        if (
+            ((index_2 < 0) and (index_0 < 2))
+            or (expfactor <= 0)
+            or ((index_2 > 0) and (index_0 >= 2))
+        ):
+            return np.nan * reference.unit
+        return reference * (1 + (index_2 / expfactor) * (2 - index_1)) ** (1 / index_2)
